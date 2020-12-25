d5bdbe89cfc3b2f875d160c686bbf768ed7d2761
Oladele Seyi <tayooladelet@gmail.com>
2020-12-25 22:25:45 +0100

    Added required cognito parameters

--- models/User.js
+++ models/User.js
@@ -1,7 +1,18 @@
 import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 const userSchema = new Schema({
-  userId: String,
+  userId: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  cognitoId: {
+    type: String,
+    required: true,
+  },
   name: String,
   email: String,
   phoneNumber: String,
